63f2615a7d5858b4e30ad69a9f5c4b1669a57dc0
정형빈 <gudqls369@gmail.com>
2022-10-06 00:49:21 +0900

    게시글 수정&삭제 기능 완성

--- post/views.py
+++ post/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.decorators import login_required
+from .forms import Postedit
 
 def main_page(request):
     posts = Post.objects.order_by('-id')
     return render(request, 'main_page.html', {'posts':posts})
 
 
+@login_required
 def make_post(request):
     return render(request, 'make_post.html')
 
+@login_required
 def create_post(request):
     user = request.user
     post = Post()
@@ -22,11 +25,12 @@ def create_post(request):
     post.save()
     return redirect('/detailed_post/' + str(post.id))
 
-
+@login_required
 def detailed_post(request, post_id):
     post_detail = get_object_or_404(Post, pk=post_id)
     return render(request, 'detailed_post.html', {'post': post_detail})
 
+@login_required
 def new_post(request):
     full_text = request.GET['fulltext']
 
@@ -44,4 +48,27 @@ def new_post(request):
 
     return render(request, 'make_post.html', {'fulltext': full_text, 'total': len(word_list), 'dictionary': word_dictionary.items()} )
 
+@login_required
+def edit_post(request, post_id):
+    post = Post.objects.get(id=post_id)
 
+    if request.method =='POST':
+        form = Postedit(request.POST)
+        if form.is_valid():
+            user = request.user
+            post.title = form.cleaned_data['title']
+            post.body = form.cleaned_data['body']
+            post.pub_date=timezone.now()
+            post.nickname = user
+            post.save()
+            return redirect('/detailed_post/' + str(post.id))
+    else:
+        form = Postedit(instance = post)
+ 
+        return render(request,'edit_post.html', {'form':form})
+
+@login_required
+def delete_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    return redirect('/')
