cec9a3cd0e18d415ebcb0e48e9fc68b255b944a9
정형빈 <gudqls369@gmail.com>
2022-10-05 19:17:14 +0900

    게시글작성기능 완성

--- feelsns/settings.py
+++ feelsns/settings.py
@@ -113,6 +113,8 @@ TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
--- users/models.py
+++ users/models.py
@@ -11,4 +11,7 @@ class UserModel(AbstractUser):
     nickname = models.TextField(max_length=16, null=False)
     bio = models.CharField(max_length=500, blank=True)
     # user_images = models.ImageField(null=True, upload_to="", blank=True)
-    # follow = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followee')
+    # follow = models.ManyToMan\login_page\yField(settings.AUTH_USER_MODEL, related_name='followee')
+        
+    def __str__(self):
+        return self.username
